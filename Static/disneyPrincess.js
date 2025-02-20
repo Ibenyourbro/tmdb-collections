6466dabb5db9a5e55a2e88d651d4339133a81005
Iben <iben.oneal@gmail.com>
2025-02-20 07:25:37 +0000

    Use specific movie list for Disney Princess catalog

---
+++ Static/disneyPrincess.js
@@ -0,0 +1,39 @@
+// List of TMDB IDs for Disney Princess movies and their collections
+module.exports = {
+  // Core Disney Princess movies
+  movies: [
+    277834,  // Moana
+    109445,  // Frozen
+    812,     // Aladdin
+    10882,   // Mulan
+    11970,   // Beauty and the Beast (1991)
+    10198,   // The Little Mermaid
+    11224,   // The Princess and the Frog
+    9325,    // Pocahontas
+    12454,   // Tangled
+    10882,   // Mulan
+    260513,  // Brave
+    49013,   // Sleeping Beauty
+    11224,   // The Princess and the Frog
+    11318,   // Snow White and the Seven Dwarfs
+    9479,    // Cinderella (1950)
+  ],
+  
+  // Keywords specifically related to Disney Princesses
+  keywords: [
+    2064,    // Princess
+    161336,  // Disney Princess
+    10148,   // Fairy Tale
+    196362,  // Princess Movie
+    241571,  // Disney Princess Film
+    179430,  // Princess Story
+    156209,  // Princess Character
+  ],
+  
+  // Collections that contain Disney Princess movies
+  collections: [
+    87800,   // Princess Collection
+    425,     // Disney Animated Feature Collection
+    338953,  // Disney Princess Collection
+  ]
+};
