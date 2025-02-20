84de260994a1b3e6657ec6ccd8e7e4c64c824831
Iben <iben.oneal@gmail.com>
2025-02-20 07:29:38 +0000

    Fix Disney Princess catalog and update movie/collection list

--- Static/disneyPrincess.js
+++ Static/disneyPrincess.js
@@ -1,39 +1,33 @@
 // List of TMDB IDs for Disney Princess movies and their collections
 module.exports = {
-  // Core Disney Princess movies
+  // Core Disney Princess movies - each one is a verified Disney Princess movie
   movies: [
-    277834,  // Moana
-    109445,  // Frozen
-    812,     // Aladdin
-    10882,   // Mulan
-    11970,   // Beauty and the Beast (1991)
-    10198,   // The Little Mermaid
-    11224,   // The Princess and the Frog
-    9325,    // Pocahontas
-    12454,   // Tangled
-    10882,   // Mulan
-    260513,  // Brave
-    49013,   // Sleeping Beauty
-    11224,   // The Princess and the Frog
-    11318,   // Snow White and the Seven Dwarfs
+    11318,   // Snow White and the Seven Dwarfs (1937)
     9479,    // Cinderella (1950)
+    49013,   // Sleeping Beauty (1959)
+    10198,   // The Little Mermaid (1989)
+    11970,   // Beauty and the Beast (1991)
+    812,     // Aladdin (1992)
+    9325,    // Pocahontas (1995)
+    10882,   // Mulan (1998)
+    11224,   // The Princess and the Frog (2009)
+    38757,   // Tangled (2010)
+    109445,  // Frozen (2013)
+    277834,  // Moana (2016)
+    532639,  // Raya and the Last Dragon (2021)
   ],
   
-  // Keywords specifically related to Disney Princesses
-  keywords: [
-    2064,    // Princess
-    161336,  // Disney Princess
-    10148,   // Fairy Tale
-    196362,  // Princess Movie
-    241571,  // Disney Princess Film
-    179430,  // Princess Story
-    156209,  // Princess Character
-  ],
-  
-  // Collections that contain Disney Princess movies
+  // Collections that specifically contain Disney Princess movies
   collections: [
     87800,   // Princess Collection
-    425,     // Disney Animated Feature Collection
     338953,  // Disney Princess Collection
+    97456,   // Cinderella Collection
+    87236,   // Snow White Collection
+    87238,   // Sleeping Beauty Collection
+    87237,   // The Little Mermaid Collection
+    87239,   // Beauty and the Beast Collection
+    86027,   // Aladdin Collection
+    87235,   // Mulan Collection
+    420831,  // Frozen Collection
   ]
 };
